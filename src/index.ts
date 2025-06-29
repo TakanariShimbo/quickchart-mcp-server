1045f57b5b91ccd23bcf96578310a32191463a44
TakanariShimbo <takanari.shimbo@gmail.com>
2025-06-30 08:57:23 +0900

    chore: release version 0.7.5

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { TOOLS, TOOL_HANDLERS } from "./tools/index.js";
 const server = new Server(
   {
     name: "quickchart-server",
-    version: "0.7.4",
+    version: "0.7.5",
   },
   {
     capabilities: {
